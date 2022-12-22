4a70527ba598d9c572c274586e9b191dffb9a94e
mishraprabhat1915 <mishraprabhat104@gmail.com>
2022-12-23 01:23:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Products from "./components/Products";
+import ipConfig from "./ipConfig.json";
+import { Route, Switch } from "react-router-dom";
+
+export const config = {
+  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <Products />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
